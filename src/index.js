712a38bdfa29331c6f7b62b8a1448bfff07428d9
Rinat Tabaev <rdtabaev@edu.hse.ru>
2023-09-18 09:57:45 +0300

    добавляет новые библиотеки, добавляет конфигурацию redux-toolkit store

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/app/app";
 import { ConstructorProvider } from "./context/ConstructorContext";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ConstructorProvider>
-      <App />
-    </ConstructorProvider>
+    <Provider store={store}>
+      <ConstructorProvider>
+        <App />
+      </ConstructorProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root"),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
